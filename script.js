5ad8cd06d24110e41f244d245d4b7ade89e866de
ghostofyoteidatabase <michaelismaelcf@yahoo.com>
2026-05-09 16:55:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Ghost of Yōtei Database Loaded");
+
+function showPage(pageId) {
+  const pages = document.querySelectorAll('.page');
+  const buttons = document.querySelectorAll('.tab-button');
+
+  pages.forEach(page => {
+    page.classList.remove('active-page');
+  });
+
+  buttons.forEach(button => {
+    button.classList.remove('active');
+  });
+
+  document.getElementById(pageId).classList.add('active-page');
+
+  event.target.classList.add('active');
+}
